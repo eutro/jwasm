cb6c134c63b0febc99b44611e1d4bcc72b986a78
eutro <benedek.szilvasy@gmail.com>
2021-03-17 11:19:11 +0000

    Render Docs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,18 @@
+plugins {
+    `java-library`
+}
+
+tasks.jar {
+    enabled = false
+}
+
 subprojects {
     apply<JavaLibraryPlugin>()
 
     group = "io.github.eutro.jwasm"
     version = "${properties["ver_major"]}.${properties["ver_minor"]}.${properties["ver_patch"]}"
 
-    extensions.configure<JavaPluginExtension>("java") {
+    java {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
@@ -14,22 +22,38 @@ subprojects {
     }
 
     dependencies {
-        "testImplementation"("org.junit.jupiter:junit-jupiter-api:5.6.2")
-        "testRuntimeOnly"("org.junit.jupiter:junit-jupiter-engine")
-        "testImplementation"(project(":jwasm-test"))
-        "implementation"("org.jetbrains:annotations:20.1.0")
+        testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
+        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+        testImplementation(project(":jwasm-test"))
+        implementation("org.jetbrains:annotations:20.1.0")
         if (path != ":jwasm") {
-            "implementation"(project(":jwasm"))
+            implementation(project(":jwasm"))
         }
     }
 
-    tasks.named<Test>("test") {
+    tasks.test {
         useJUnitPlatform()
     }
 }
 
 project(":jwasm-test") {
     dependencies {
-        "implementation"("org.junit.jupiter:junit-jupiter-api:5.6.2")
+        implementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
     }
 }
+
+val javadocModules = listOf(":jwasm", ":jwasm-tree")
+
+tasks.register<Copy>("assembleDocs") {
+    val javadocTasks = javadocModules.map { project(it).tasks.javadoc.get() }.toTypedArray()
+    dependsOn(*javadocTasks)
+    delete("docs")
+    javadocTasks.forEach { jdoc ->
+        from(jdoc) {
+            into(jdoc.project.name)
+        }
+    }
+    into("docs")
+}
+
+defaultTasks("build", "assembleDocs")
