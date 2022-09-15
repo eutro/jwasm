572a885eadae9ad01d9fafb810ad936c2a402b7c
eutro <benedek.szilvasy@gmail.com>
2022-09-15 23:53:06 +0200

    Replace LinkedList in ExprNode with ArrayList (and erase to List type in API) ArrayList has a much lower memory footprint, so it should be better in the general use-case. Bump minor version since it's technically a breaking API change.

--- jwasm-tree/src/main/java/io/github/eutro/jwasm/tree/ExprNode.java
+++ jwasm-tree/src/main/java/io/github/eutro/jwasm/tree/ExprNode.java
@@ -5,9 +5,7 @@ import io.github.eutro.jwasm.ExprVisitor;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.LinkedList;
+import java.util.*;
 
 /**
  * A node that represents an
@@ -17,7 +15,7 @@ public class ExprNode extends ExprVisitor implements Iterable<AbstractInsnNode> 
     /**
      * The list of {@link AbstractInsnNode instructions} in this expression, or {@code null} if there aren't any.
      */
-    public @Nullable LinkedList<AbstractInsnNode> instructions;
+    public @Nullable List<AbstractInsnNode> instructions;
 
     /**
      * Construct a visitor with no delegate.
@@ -48,123 +46,123 @@ public class ExprNode extends ExprVisitor implements Iterable<AbstractInsnNode> 
         ev.visitEnd();
     }
 
-    private LinkedList<AbstractInsnNode> insns() {
-        if (instructions == null) instructions = new LinkedList<>();
+    private List<AbstractInsnNode> insns() {
+        if (instructions == null) instructions = new ArrayList<>();
         return instructions;
     }
 
     @Override
     public void visitInsn(byte opcode) {
         super.visitInsn(opcode);
-        insns().addLast(new InsnNode(opcode));
+        insns().add(new InsnNode(opcode));
     }
 
     @Override
     public void visitPrefixInsn(int opcode) {
         super.visitPrefixInsn(opcode);
-        insns().addLast(new PrefixInsnNode(opcode));
+        insns().add(new PrefixInsnNode(opcode));
     }
 
     @Override
     public void visitConstInsn(Object v) {
         super.visitConstInsn(v);
-        insns().addLast(new ConstInsnNode(v));
+        insns().add(new ConstInsnNode(v));
     }
 
     @Override
     public void visitNullInsn(byte type) {
         super.visitNullInsn(type);
-        insns().addLast(new NullInsnNode(type));
+        insns().add(new NullInsnNode(type));
     }
 
     @Override
     public void visitFuncRefInsn(int function) {
         super.visitFuncRefInsn(function);
-        insns().addLast(new FuncRefInsnNode(function));
+        insns().add(new FuncRefInsnNode(function));
     }
 
     @Override
     public void visitSelectInsn(byte[] type) {
         super.visitSelectInsn(type);
-        insns().addLast(new SelectInsnNode(type));
+        insns().add(new SelectInsnNode(type));
     }
 
     @Override
     public void visitVariableInsn(byte opcode, int variable) {
         super.visitVariableInsn(opcode, variable);
-        insns().addLast(new VariableInsnNode(opcode, variable));
+        insns().add(new VariableInsnNode(opcode, variable));
     }
 
     @Override
     public void visitTableInsn(byte opcode, int table) {
         super.visitTableInsn(opcode, table);
-        insns().addLast(new TableInsnNode(opcode, table));
+        insns().add(new TableInsnNode(opcode, table));
     }
 
     @Override
     public void visitPrefixTableInsn(int opcode, int table) {
         super.visitPrefixTableInsn(opcode, table);
-        insns().addLast(new PrefixTableInsnNode(opcode, table));
+        insns().add(new PrefixTableInsnNode(opcode, table));
     }
 
     @Override
     public void visitPrefixBinaryTableInsn(int opcode, int firstIndex, int secondIndex) {
         super.visitPrefixBinaryTableInsn(opcode, firstIndex, secondIndex);
-        insns().addLast(new PrefixBinaryTableInsnNode(opcode, firstIndex, secondIndex));
+        insns().add(new PrefixBinaryTableInsnNode(opcode, firstIndex, secondIndex));
     }
 
     @Override
     public void visitMemInsn(byte opcode, int align, int offset) {
         super.visitMemInsn(opcode, align, offset);
-        insns().addLast(new MemInsnNode(opcode, align, offset));
+        insns().add(new MemInsnNode(opcode, align, offset));
     }
 
     @Override
     public void visitIndexedMemInsn(int opcode, int index) {
         super.visitIndexedMemInsn(opcode, index);
-        insns().addLast(new IndexedMemInsnNode(opcode, index));
+        insns().add(new IndexedMemInsnNode(opcode, index));
     }
 
     @Override
     public void visitBlockInsn(byte opcode, BlockType blockType) {
         super.visitBlockInsn(opcode, blockType);
-        insns().addLast(new BlockInsnNode(opcode, blockType));
+        insns().add(new BlockInsnNode(opcode, blockType));
     }
 
     @Override
     public void visitElseInsn() {
         super.visitElseInsn();
-        insns().addLast(new ElseInsnNode());
+        insns().add(new ElseInsnNode());
     }
 
     @Override
     public void visitEndInsn() {
         super.visitEndInsn();
-        insns().addLast(new EndInsnNode());
+        insns().add(new EndInsnNode());
     }
 
     @Override
     public void visitBreakInsn(byte opcode, int label) {
         super.visitBreakInsn(opcode, label);
-        insns().addLast(new BreakInsnNode(opcode, label));
+        insns().add(new BreakInsnNode(opcode, label));
     }
 
     @Override
     public void visitTableBreakInsn(int[] labels, int defaultLabel) {
         super.visitTableBreakInsn(labels, defaultLabel);
-        insns().addLast(new TableBreakInsnNode(labels, defaultLabel));
+        insns().add(new TableBreakInsnNode(labels, defaultLabel));
     }
 
     @Override
     public void visitCallInsn(int function) {
         super.visitCallInsn(function);
-        insns().addLast(new CallInsnNode(function));
+        insns().add(new CallInsnNode(function));
     }
 
     @Override
     public void visitCallIndirectInsn(int table, int type) {
         super.visitCallIndirectInsn(table, type);
-        insns().addLast(new CallIndirectInsnNode(table, type));
+        insns().add(new CallIndirectInsnNode(table, type));
     }
 
     @NotNull
